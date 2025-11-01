34807246d609d22e5aa64ae8219135a72fec396b
anak LSP-Bisnis <jonathan.1415064@penabur.sch.id>
2025-11-01 20:55:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 var style = document.createElement('style');
 style.innerHTML = `
     body {
-        font-family: 'Cinzel', 'Times New Roman', serif;
+        font-family: 'Mulish', 'Times New Roman', serif;
     }
     .title, .section, .cookieNumber {
         font-family: 'UnifrakturMaguntia', cursive;
     }
     .description, .price, .warning, .option {
-        font-family: 'Cinzel', 'Times New Roman', serif;
+        font-family: 'Mulish', 'Times New Roman', serif;
     }
 `;
 document.head.appendChild(style);
@@ -16,5 +16,5 @@ document.head.appendChild(style);
 // Tambahkan link font di header (biasanya dilakukan di HTML, tapi bisa lewat JavaScript)
 var link1 = document.createElement('link');
 link1.rel = 'stylesheet';
-link1.href = 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=UnifrakturMaguntia&display=swap';
+link1.href = 'https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&family=UnifrakturMaguntia&display=swap';
 document.head.appendChild(link1);
